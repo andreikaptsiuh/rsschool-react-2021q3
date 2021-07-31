e2f0e03d4ece00be80c558146796bc989e96f55e
Andrei Kaptsiuh <kaptsiuhandrei@gmail.com>
2021-07-31 13:36:52 +0300

    feat: add cards img

--- react-components/src/components/cards-board/card.tsx
+++ react-components/src/components/cards-board/card.tsx
@@ -4,12 +4,19 @@ type CardProps = {
   card: {
     title: string,
     description: string,
+    src: string,
+    price: number,
   }
 }
 
 export const Card: React.FC<CardProps> = (props: CardProps) => (
   <div className="card">
-    <h3>{props.card.title}</h3>
-    <p>{props.card.description}</p>
+    <img className="card_img" src={props.card.src} alt="img" />
+    <h3 className="card_title">{props.card.title}</h3>
+    <p className="card_price">
+      Price:
+      {props.card.price}
+    </p>
+    <p className="card_description">Click to bye!</p>
   </div>
 );
