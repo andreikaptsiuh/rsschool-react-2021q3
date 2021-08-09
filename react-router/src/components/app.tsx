93a338638eac43e8e13070dad0c33f8ee908f708
Andrei Kaptsiuh <kaptsiuhandrei@gmail.com>
2021-08-09 12:38:51 +0300

    feat: add router

--- react-router/src/components/app.tsx
+++ react-router/src/components/app.tsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+} from 'react-router-dom';
+import { Header } from './header';
+import { Home } from './pages/home';
+import { About } from './pages/about';
 
 const App: React.FC = () => (
   <>
-    <h1>App</h1>
+    <BrowserRouter>
+      <Header />
+
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   </>
 );
 
---
+++ react-router/src/components/pages/about.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const About: React.FC = () => (
+  <h2>About page</h2>
+);
