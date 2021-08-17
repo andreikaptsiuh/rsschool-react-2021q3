6f0f227a10dc931c5cb3f68e8b61416becd042dd
Andrei Kaptsiuh <kaptsiuhandrei@gmail.com>
2021-08-17 20:21:10 +0300

    fix: reducer

--- react-router-redux/src/shared/store/store.ts
+++ react-router-redux/src/shared/store/store.ts
@@ -1,10 +1,12 @@
-import { applyMiddleware, createStore, combineReducers } from 'redux';
-import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { combineReducers } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 import { appReducer } from './appReducer';
 
 const rootReducer = combineReducers({
   app: appReducer,
 });
 
-export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+export const store = configureStore({
+  reducer: rootReducer,
+  devTools: process.env.NODE_ENV !== 'production',
+});
